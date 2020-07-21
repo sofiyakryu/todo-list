6f4d4385a961acc5b50252f9be682edbb982c1d0
sofiyakryu <63737183+sofiyakryu@users.noreply.github.com>
2020-07-21 15:44:53 +0100

    Create layout and node module

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,38 +1,64 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 let items = ["Buy food", "Cook food", "Eat food"];
+let workItems = [];
+let shoppingItems = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  let today = new Date();
+let day = date();
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString('en-GB', options);
-
-
-  res.render("list", {kindOfDay: day, newListItems: items});
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
 
 });
 
 app.post("/", function(req, res) {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
-})
 
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else if (req.body.list === "Shopping") {
+    shoppingItems.push(item);
+    res.redirect("/shopping")
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.get("/shopping", function(req, res) {
+  res.render("list", {
+    listTitle: "Shopping List",
+    newListItems: shoppingItems
+  });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
